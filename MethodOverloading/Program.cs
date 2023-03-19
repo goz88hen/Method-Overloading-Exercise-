388a5344149be7f4ebd5de3563cfff87dfe5101c
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-03-19 04:24:00 -0700

    Method Overloading project has been completed.

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace MethodOverloading
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
+using MethodOverloading;
+using System.ComponentModel;
+
+var answer1 =Methods.Add(2, 2);
+//var answer2 = Methods.Add(2.333, 2.560);
+//var answer3 = Methods.Add(2.5m, .237m);
+//var answer4 = Methods.Add(5, 20, true or false);
+Console.WriteLine(answer1); //change to answer(1,2,3 or 4) in order to get the distinctive answer.
+
+
